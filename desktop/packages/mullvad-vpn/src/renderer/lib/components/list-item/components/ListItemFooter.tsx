435e2fc69034499809a40788123dda7e452cec71
Oliver <oliver@mohlin.dev>
2025-02-20 09:32:33 +0100

    Add ListItem component

---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/ListItemFooter.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+import { Spacings } from '../../../foundations';
+import { Flex } from '../../flex';
+
+export const ListItemFooter = styled(Flex).attrs({
+  $padding: { horizontal: Spacings.spacing5 },
+})``;
---
+++ desktop/packages/mullvad-vpn/src/renderer/lib/components/list-item/components/ListItemGroup.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import { Spacings } from '../../../foundations';
+import { Flex } from '../../flex';
+
+export const ListItemGroup = styled(Flex).attrs({
+  $alignItems: 'center',
+  $gap: Spacings.spacing3,
+})``;
